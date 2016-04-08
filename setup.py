16e5f35a68a3c347fe87a3f91213bc5d7f7b6e07
Guilherme Polo <gp@instaprint.me>
2016-04-08 15:56:15 -0500

    forgot to include setup.py :)

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ccv",
+    version="0.0.1",
+
+    description='FFI bindings to libccv',
+    long_description=open('README.md').read(),
+    url='https://github.com/gpip/py-ccv',
+    author='Guilherme Polo',
+    author_email='gp@instaprint.me',
+
+    setup_requires=['cffi>=1.3.0', 'pytest-runner==2.6.2'],
+    install_requires=['cffi>=1.3.0'],
+    tests_require=['pytest==2.8.7'],
+
+    packages=find_packages(),
+    cffi_modules=[
+        "build_wrapper.py:ffi"
+    ],
+    zip_safe=False,
+
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Software Development :: Libraries"
+    ]
+)
